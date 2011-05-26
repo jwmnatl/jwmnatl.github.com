6cf9b667e1e0cdd2619c5adae8a337c4e5ed05b9
Jimmy Moore <jwmnatl@gmail.com>
2011-05-26 09:03:25 -0400

    Chapter 10 Exercises

---
+++ python101/chapter10.py
@@ -0,0 +1,43 @@
+### Chapter 10 (Tuples): Python 101 :Jimmy Moore ###
+
+# Ex.10.1
+"""
+file = raw_input('Enter a file name: ')
+try:
+	fhand = open(file)
+except:
+	print 'File cannot be opened: ', file
+	exit()	
+emails = dict()
+for line in fhand:
+	words = line.split()
+	# print 'Debug', words
+	if len(words) == 0 or words[0] != 'From' : continue
+	emails[words[1]] = emails.get(words[1], 0) + 1	
+	lst = []
+	for key, val in sorted(emails.items()):
+		lst.append((val,key))
+	lst.sort(reverse=True)
+for key, val in lst[:1]:
+	print key, val
+"""
+# Ex.10.2
+
+file = raw_input('Enter a file name: ')
+try:
+	fhand = open(file)
+except:
+	print 'File cannot be opened: ', file
+	exit()	
+emails = dict()
+for line in fhand:
+	words = line.split()
+	# print 'Debug', words
+	if len(words) == 0 or words[0] != 'From' : continue
+	hour = words[5].split(':')
+	emails[hour[0]] = emails.get(hour[0], 0) + 1	
+	lst = []
+	for key, val in sorted(emails.items()):
+		lst.append((val,key))
+for key, val in lst:
+	print val, key
